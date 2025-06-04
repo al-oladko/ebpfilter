8ddd8a6750de11648b83e13c988b81c95f625316
Aleksei Oladko <aleks.oladko@gmail.com>
2025-06-04 21:32:54 +0200

    Atomic operations

---
+++ src/atomic.h
@@ -0,0 +1,24 @@
+#pragma once
+
+static __always_inline __s64 atomic_read(__s64 *v)
+{
+	return (__s64)*(volatile __s64 *)v;
+}
+
+static __always_inline __s64 atomic64_sub_return(__s64 *v, int sub)
+{
+	return __sync_sub_and_fetch(v, sub);
+	*v -= sub;
+	return *v;
+}
+
+static __always_inline __s64 atomic64_add_return(__s64 *v, int add)
+{
+	return __sync_add_and_fetch(v, add);
+}
+
+static __always_inline __s64 atomic64_cmpxchg(__s64 *v, __s64 old, __s64 new)
+{
+	return __sync_val_compare_and_swap(v, old, new);
+}
+
